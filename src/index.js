739a270640d91779b5147bf57691f0270da90706
dangrammer <danieljromans@gmail.com>
2020-07-08 14:33:30 -0400

    adjusted nesting in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {ActionCableProvider} from 'react-actioncable-provider'
 import {Provider} from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
-///
-import {ActionCableProvider} from 'react-actioncable-provider'
 import {API_WS_ROOT} from './constants'
-//
 import configureStore from './configureStore'
 import App from './App'
 import './css/index.css'
@@ -13,13 +11,12 @@ import './css/index.css'
 const store = configureStore()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      {/* <App/> */}
-      <ActionCableProvider url={API_WS_ROOT}>
-        <App />
-      </ActionCableProvider>
-    </Router>
-  </Provider>,
+  <ActionCableProvider url={API_WS_ROOT}>
+    <Provider store={store}>
+      <Router>
+        <App/>
+      </Router>
+    </Provider>
+  </ActionCableProvider>,
   document.querySelector('#root')
 )
