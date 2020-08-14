b782339500fbfd4cc763ce1c6b1ea97b8215135a
dangrammer <danieljromans@gmail.com>
2020-08-14 14:54:43 -0400

    added middleware logic to cable.js

--- src/cable.js
+++ src/cable.js
@@ -1,3 +1,23 @@
 import ActionCable from 'actioncable'
+import API_WS_ROOT from './constants'
 
-console.log(ActionCable)
+const cable = () => {
+  const cable = ActionCable.createConsumer(API_WS_ROOT)
+
+  return (dispatch) => next => (action) => {
+    if (typeof(action) === 'function') return next(action)
+
+    const {channel} = action
+    let {received} = action
+
+    if (!channel) return next(action)
+
+    if (typeof received === 'string') {
+      received = result => dispatch({type: received, result})
+    }
+
+    return cable.subscriptions.create({channel}, {received})
+  }
+}
+
+export default cable
